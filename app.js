aa5bcf53332811b564b69613f7cb9f450d36d00f
Franco Lippi <soyflippi@gmail.com>
2021-04-12 14:13:02 -0300

    update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const db = require('./models/db')
 
+// Init DB
 db.connect((err) => {
     if (err) {
         console.log(err);
@@ -14,22 +14,21 @@ db.connect((err) => {
     console.log('MonngoDB Connected');
 })
 
+// Cors config
 const corsOptions = {
     origin: "*"
 };
 
+// Init express app
 const app = express();
 
 // Middleware for logged requests in console
 app.use(morgan('tiny'))
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Routes
 app.use('/', require('./routes/index'))
 app.use('/user', require('./routes/user'))
 
